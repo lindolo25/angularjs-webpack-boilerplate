83038f3864cb8502695f2e834997a7e752c80667
Rolando Perez <rolando@everymundo.com>
2019-12-03 12:08:03 -0500

    added loader for json files.

--- src/services/people.service.js
+++ src/services/people.service.js
@@ -1,31 +1,12 @@
 'use strict';
 
+import people from "./people.json";
+
 peopleFactory.$inject = ['$q', '$log'];
 
 export default peopleFactory;
 
 function peopleFactory($q, $log) {
-
-    const people = [{
-        name: 'Steve Rogers',
-        age: 31
-    },
-    {
-        name: 'Tony Stark',
-        age: 42
-    },
-    {
-        name: 'Thor',
-        age: 35
-    },
-    {
-        name: 'Bruce Bennett',
-        age: 45
-    },
-    {
-        name: 'Natasha Romanoff',
-        age: 29
-    }];
 
     return {
         findAll,
