0002bad0ff8afb54f711c74ee3eb9f68c94251dd
Rolando Perez <rolando@everymundo.com>
2019-12-12 12:48:16 -0500

    extended webpack configuration added new plugins and rules.

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,85 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
-    entry: './src/app.js',
-    mode: 'development',
-    devtool: 'inline-source-map',
-    output: {
-        filename: 'bundle.js',
-        chunkFilename: "[name].[chunkhash].js",
-        path: path.resolve(__dirname, '.dist')
-    },
-    devServer: {
-        contentBase: __dirname,
-        port: 3000
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(html)$/,
-                use: {
-                    loader: 'html-loader',
-                    options: {
-                        attrs: [':data-src']
+const devServer = {
+    contentBase: __dirname,
+    port: 3000
+};
+
+module.exports = (env) => {
+
+    const config = {
+        entry: './src/app.js',
+        mode: 'development',
+        devtool: 'inline-source-map',
+        output: {
+            filename: 'bundle.js',
+            chunkFilename: "[name].[chunkhash].js",
+            path: path.resolve(__dirname, '.dist')
+        },
+        module: {
+            rules: [
+                {
+                    enforce: "pre",
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: "eslint-loader",
+                },
+                {
+                    test: /\.(html)$/,
+                    use: {
+                        loader: 'html-loader',
+                        options: {
+                            attrs: [':data-src']
+                        }
                     }
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader']
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader'],
                 }
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: ['babel-loader']
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            }
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                filename: "index.html",
+                template: path.resolve("./index.html")
+            })
         ]
+    };
+
+    if (env && env.dev) {
+        config.devServer = devServer;
+        config.plugins.push(
+            new webpack.NamedModulesPlugin(),
+            new webpack.HotModuleReplacementPlugin()
+        );
     }
+
+    if (env && env.production) {
+        config.devtool = "source-map";
+        config.plugins.push(
+            new UglifyJSPlugin({
+                uglifyOptions: {
+                    warnings: false,
+                    output: { comments: false }
+                },
+                sourceMap: true
+            }),
+            new webpack.DefinePlugin({
+                "process.env.NODE_ENV": JSON.stringify("production")
+            })
+        );
+    }
+
+    return config;
 };
