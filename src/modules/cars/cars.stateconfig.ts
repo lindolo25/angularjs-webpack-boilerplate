8b2868dcfcee76c669ca02bbd753951aeabd90c7
Rolando Perez <rolando@everymundo.com>
2020-05-18 19:21:18 -0400

    added name for chunk file.

--- src/modules/cars/cars.stateconfig.ts
+++ src/modules/cars/cars.stateconfig.ts
@@ -3,5 +3,5 @@ import { NgHybridStateDeclaration } from '@uirouter/angular-hybrid';
 export const carsState: NgHybridStateDeclaration = {
     name: 'cars.**',
     url: '/cars',
-    loadChildren: () => import('./cars.module').then(m => m.CarsModule),
+    loadChildren: () => import(/* webpackChunkName: "cars.module" */ './cars.module').then(m => m.CarsModule),
 };
