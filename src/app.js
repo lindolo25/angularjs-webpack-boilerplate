311c34ec1e13c8d11831e0c9f686674870a3178a
Rolando Perez <rolando@everymundo.com>
2020-05-18 18:46:58 -0400

    updated app to import angular2BoostrapModule. created a document ready event to manually boostrap angularjs and angular apps and modules.

--- src/app.js
+++ src/app.js
@@ -7,14 +7,38 @@ import 'oclazyload';
 import appModules from './modules';
 import services from './services/services.module';
 
+import { Angular2BootstrapModule } from './modules/boostrap.module';
+import { UpgradeModule } from '@angular/upgrade/static';
+import { UrlService } from '@uirouter/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
 angular.module('app', [
 	appModules.name,
 	services.name,
 	'oc.lazyLoad',
-	'ui.router', 
 	'ui.router.upgrade'
 ])
 	.constant('appName', 'Angularjs-Webpack-Example')
 	.config(['$urlRouterProvider', function ($urlRouterProvider) {
 		$urlRouterProvider.otherwise('/');
-	}]);
+	}])
+	.config(['$urlServiceProvider', ($urlService) => $urlService.deferIntercept()]);
+
+// Wait until the DOM is ready
+angular.element(document).ready(function () {
+	// Manually bootstrap the Angular app
+	platformBrowserDynamic()
+		.bootstrapModule(Angular2BootstrapModule)
+		.then(platformRef => {
+			
+			const injector = platformRef.injector;
+			const upgrade = injector.get(UpgradeModule);
+			// Manually bootstrap the AngularJS app
+			upgrade.bootstrap(document.body, ['app']);
+			// Intialize the Angular Module (get() any UIRouter service from DI to initialize it)
+			const url = injector.get(UrlService);
+			// Instruct UIRouter to listen to URL changes
+			url.listen();
+			url.sync();
+		});
+});
