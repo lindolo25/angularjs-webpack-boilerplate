77aacee0fdfb2195673df4deeca76eed503d0a21
Rolando Perez <rolando@everymundo.com>
2020-05-19 12:24:49 -0400

    created resolver fo cars in carsList state.

--- src/modules/cars/carsList/carsList.stateconfig.ts
+++ src/modules/cars/carsList/carsList.stateconfig.ts
@@ -1,8 +1,16 @@
 import { Ng2StateDeclaration } from "@uirouter/angular";
 import { CarsList } from "./carsList.component";
+import { CarsService } from "../../../services/cars.service";
 
 export const carsListState: Ng2StateDeclaration = {
     name: "cars",
     url: "/cars",
+    resolve: [{
+        token: 'cars',
+        deps: [CarsService],
+        resolveFn: function (carsService: CarsService) {
+            return carsService.getCars();
+        }
+    }],
     component: CarsList
 };
