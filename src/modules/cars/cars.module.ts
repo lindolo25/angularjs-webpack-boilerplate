0ed88534e470bbe8100a168bd6719cc1ff913ca4
Rolando Perez <rolando@everymundo.com>
2020-05-19 14:05:26 -0400

    added CommonModule to CarsModule.

--- src/modules/cars/cars.module.ts
+++ src/modules/cars/cars.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { UIRouterUpgradeModule } from "@uirouter/angular-hybrid";
+import { CommonModule } from '@angular/common';
 
 import { carsListState } from "./carsList/carsList.stateconfig";
 import { CarsList } from "./carsList/carsList.component";
 
 @NgModule({
 	imports: [
+		CommonModule,
 		UIRouterUpgradeModule.forChild({ states: [carsListState] }),
 	],
 	declarations: [CarsList],
