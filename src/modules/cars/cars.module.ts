6ff95e80c43f99ce9d9c4cff0535767bf321e5f4
Rolando Perez <rolando@everymundo.com>
2020-05-19 12:26:07 -0400

    implemented OnInit and added CarsService as dependency to CasList component.

--- src/modules/cars/cars.module.ts
+++ src/modules/cars/cars.module.ts
@@ -4,11 +4,10 @@ import { UIRouterUpgradeModule } from "@uirouter/angular-hybrid";
 import { carsListState } from "./carsList/carsList.stateconfig";
 import { CarsList } from "./carsList/carsList.component";
 
-/** The NgModule for Contacts feature */
 @NgModule({
-  imports: [
-      UIRouterUpgradeModule.forChild({ states: [carsListState] }),
-  ],
-  declarations: [CarsList],
+	imports: [
+		UIRouterUpgradeModule.forChild({ states: [carsListState] }),
+	],
+	declarations: [CarsList],
 })
-export class CarsModule {}
+export class CarsModule { }
--- src/modules/cars/carsList/carsList.component.ts
+++ src/modules/cars/carsList/carsList.component.ts
@@ -1,5 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import template from "./carsList.view.html";
+import { CarsService } from '../../../services/cars.service';
 
 /**
  * This component renders the contacts submodule.
@@ -8,7 +9,16 @@ import template from "./carsList.view.html";
  * On the right is the ui-view viewport where contact details appear.
  */
 @Component({
-  selector: 'cars',
-  template: template
+	selector: 'cars',
+	template: template
 })
-export class CarsList {}
+export class CarsList implements OnInit {
+	
+	@Input() cars;
+
+	constructor (private carsService: CarsService) { }
+
+	ngOnInit () {
+		console.log(this.cars);
+	}
+}
