8417340c279f8f312a6f7c24cd6f62be57bbe1f9
Rolando Perez <rolando@everymundo.com>
2020-05-19 15:55:32 -0400

    added carDetail state and component to module declarations.

--- src/modules/cars/cars.module.ts
+++ src/modules/cars/cars.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 
 import { carsListState } from "./carsList/carsList.stateconfig";
 import { CarsList } from "./carsList/carsList.component";
+import { carDetailState } from './carDetail/carDetail.stateconfig';
+import { CarDetail } from './carDetail/carDetail.component';
 
 @NgModule({
 	imports: [
 		CommonModule,
-		UIRouterUpgradeModule.forChild({ states: [carsListState] }),
+		UIRouterUpgradeModule.forChild({ states: [carsListState, carDetailState] }),
 	],
-	declarations: [CarsList],
+	declarations: [CarsList, CarDetail],
 })
 export class CarsModule { }
