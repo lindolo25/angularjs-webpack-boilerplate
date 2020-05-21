08932869af83571113680a8f1b0646e45f7eac60
Rolando Perez <rolando@everymundo.com>
2020-05-21 16:45:52 -0400

    updated carDetail component to use valueEditor component and implemented update functionality.

--- src/modules/cars/carDetail/carDetail.component.ts
+++ src/modules/cars/carDetail/carDetail.component.ts
@@ -1,13 +1,28 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import template from "./carDetail.view.html";
+import { ICar } from '../../../interfaces/ICar';
 
 @Component({
 	selector: 'carDetail',
 	template: template
 })
-export class CarDetail {
+export class CarDetail implements OnInit {
 	
-    @Input() car;
+	@Input('car') pristine: ICar;
+	
+	car: ICar;
+	edit: Boolean = false;
     
 	constructor () { }
+
+	ngOnInit() {
+		this.car = { ...this.pristine };
+	}
+
+	save(car: ICar): void {
+		this.pristine.make = car.make;
+		this.pristine.model = car.model;
+		this.pristine.year = car.year;
+		this.edit = false;
+	}
 }
