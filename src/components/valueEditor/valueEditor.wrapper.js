19ed23ed344ce64cdd8414be91a565eb880255ea
Rolando Perez <rolando@everymundo.com>
2020-05-21 15:52:53 -0400

    as ngModel ctrl require is not supported by Angular, a wrapper component to be used bu Agular is necesary.

---
+++ src/components/valueEditor/valueEditor.wrapper.js
@@ -0,0 +1,8 @@
+export default {
+	bindings: {
+		text: '@',
+		model: '=',
+		required: '<?'
+	},
+	template: '<value-editor text="{{ $ctrl.text }}" ng-model="$ctrl.model" ng-required="$ctrl.required" />',
+};
