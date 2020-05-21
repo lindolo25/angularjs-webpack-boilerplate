cc1e76f0661e714ab23a3713921bda1f0093a3ba
Rolando Perez <rolando@everymundo.com>
2020-05-21 16:44:21 -0400

    added upgraded component to the carsModule declarations

--- src/modules/cars/cars.module.ts
+++ src/modules/cars/cars.module.ts
@@ -7,12 +7,13 @@ import { CarsList } from "./carsList/carsList.component";
 import { carDetailState } from './carDetail/carDetail.stateconfig';
 import { CarDetail } from './carDetail/carDetail.component';
 import { AppLabelDirective } from '../../components/appLabel/appLabelUpgrade.component';
+import { ValueEditorDirective } from '../../components/valueEditor/valueEditor.upgrade';
 
 @NgModule({
 	imports: [
 		CommonModule,
 		UIRouterUpgradeModule.forChild({ states: [carsListState, carDetailState] }),
 	],
-	declarations: [CarsList, CarDetail, AppLabelDirective],
+	declarations: [CarsList, CarDetail, AppLabelDirective, ValueEditorDirective],
 })
 export class CarsModule { }
