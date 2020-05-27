084182ca23a5dddf065b643b675e2a5dadb260e4
Rolando Perez <rolando@everymundo.com>
2020-05-27 14:10:16 -0400

    added HttpClientModule import to angular2BoostrapModule.

--- src/angular2Boostrap.module.ts
+++ src/angular2Boostrap.module.ts
@@ -4,6 +4,7 @@ import 'zone.js';
 import { NgModule } from "@angular/core";
 import { UpgradeModule } from '@angular/upgrade/static';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { UIRouterUpgradeModule } from '@uirouter/angular-hybrid';
 import { carsState } from './modules/cars/cars.stateconfig';
 
@@ -11,6 +12,7 @@ import { carsState } from './modules/cars/cars.stateconfig';
     imports: [
         BrowserModule, 
         UpgradeModule, 
+        HttpClientModule,
         UIRouterUpgradeModule,
         UIRouterUpgradeModule.forRoot({ states: [carsState] }),
     ]
