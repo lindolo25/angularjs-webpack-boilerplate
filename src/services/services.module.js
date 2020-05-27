d36ec63f92d598ceb4316f2c05d036b518a76a5d
Rolando Perez <rolando@everymundo.com>
2020-05-27 15:06:35 -0400

    added a downgraded version of GithubUsersApiService to AngularJS app.services module. this allows Angular Injectables to be injected in angularJS components.

--- src/services/services.module.js
+++ src/services/services.module.js
@@ -1,15 +1,15 @@
 'use strict';
 
 import angular from 'angular';
-// import { downgradeInjectable } from '@angular/upgrade/static';
+import { downgradeInjectable } from '@angular/upgrade/static';
 
 import peopleFactory from './people.service';
 import githubReposApiFactory from './githubReposApi.service';
-// import { GithubUsersApiService } from './githubUsersApi.service';
+import { GithubUsersApiService } from './githubUsersApi.service';
 
 let services = angular.module('app.services', [])
 	.factory('app.peopleService', peopleFactory)
-	.factory('app.githubReposApiService', githubReposApiFactory);
-	// .factory('app.githubUsersApiService', downgradeInjectable(GithubUsersApiService));
+	.factory('app.githubReposApiService', githubReposApiFactory)
+	.factory('app.githubUsersApiService', downgradeInjectable(GithubUsersApiService));
 
 export default services;
