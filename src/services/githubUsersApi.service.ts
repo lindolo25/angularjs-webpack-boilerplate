4e25c88e86fc7d4c1ace4c66ccb6d8988483b9d5
Rolando Perez <rolando@everymundo.com>
2020-05-27 17:32:20 -0400

    fixed implementation of getUser under GithubUsersApiService.

--- src/services/githubUsersApi.service.ts
+++ src/services/githubUsersApi.service.ts
@@ -5,17 +5,15 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class GithubUsersApiService {
 
-    url = 'https://api.github.com/users';
+    private url = 'https://api.github.com/users';
 
     constructor (private http: HttpClient) {
         console.log('error');
     }
 
     getUser (username: string): Promise<any> {
-        return this.http.get(`${this.url}/${username}`)
-            .toPromise()
-            .then(res => {
-                return res;
-            });
+        return this.http
+            .get(`${this.url}/${username}`)
+            .toPromise();
     }
 }
