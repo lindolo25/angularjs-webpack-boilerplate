8c70562297af55affc6ddae49af0574e4d407ad6
Rolando Perez <rolando@everymundo.com>
2020-05-28 18:04:19 -0400

    added GithubRepo component to module declarations.

--- src/components/ng2components.module.ts
+++ src/components/ng2components.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from "@angular/core";
+import { CommonModule } from '@angular/common';
 import { GithubProfile } from './profile/profile.component';
+import { GithubRepo } from './repo/repo.component';
 
-const components = [ GithubProfile ]
+const components = [ GithubProfile, GithubRepo ]
 
 @NgModule({
+    imports: [
+        CommonModule
+    ],
     declarations: components,
     entryComponents: components
 })
