d92aebaabdc74a3e20f7239385b658ffb76fab7a
Rolando Perez <rolando@everymundo.com>
2020-05-28 16:54:44 -0400

    added downgraded version of GithubProfile component to the app.components module.

--- src/components/components.module.js
+++ src/components/components.module.js
@@ -5,9 +5,13 @@ import appLabelComponent from './appLabel/appLabel.component';
 import valueEditorComponent from './valueEditor/valueEditor.component';
 import valueEditorWrapper from './valueEditor/valueEditor.wrapper';
 
+import { downgradeComponent } from '@angular/upgrade/static';
+import { GithubProfile } from './profile/profile.component';
+
 let componentsModule = angular.module('app.components', [])
 	.component('appLabel', appLabelComponent)
 	.component('valueEditor', valueEditorComponent)
-	.component('valueEditorWrapper', valueEditorWrapper);
+	.component('valueEditorWrapper', valueEditorWrapper)
+	.directive('githubProfile', downgradeComponent({ component: GithubProfile }));
 
 export default componentsModule;
